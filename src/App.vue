5da31ee07e01d8e902f8b269b205b1ac0e94d428
BLACK079 <148823174+BLACK079@users.noreply.github.com>
2023-10-25 00:06:50 +0000

    Agregando bloqueador

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,68 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue';
+const header = ref('App Lista de compras');
+const items = ref([
+  // { id: 1, label: '10 bolillos' },
+  // { id: 2, label: '1 lata de frijoles' },
+  // { id: 3, label: '2 lata de atún' }
+]);
+const saveItem = () => {
+  items.value.push({ id: items.value.length + 1, label: newItem.value })
+  //Limpiando el contenido de newItem
+  newItem.value = ""; 
+};
+const newItem = ref('');
+const newItemHighPriority = ref(false);
+const editing = ref(false);
+const doEdit = (edit) => {
+  //alteramos la variable esditing
+  editing.value = edit;
+  // limpiamos el input de texto
+  newItem.value = ""; 
+};
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="header">
+    <h1> <i class="material-icons shopping-cart-icon">local_mall</i> {{ header }}</h1>
+    <button v-if="!editing" @click="doEdit(true)" class="btn btn-primary">Agregar</button>
+    <button v-else @click="doEdit(false)" class="btn btn-cancel">Cancelar</button>
+  </div>
+  <!-- <a v-bind:href="newItem">
+    <i class="material-icons shopping-cart-icon">link</i>
+  </a> -->
+  <form v-if="editing" v-on:submit.prevent= saveItem class="add-item form">
+    <!-- Input de nuevo articulo -->
+    <input class="addint" v-model.trim="newItem" type="text" placeholder="Ingresar articulo" required>
+    <!-- Check Boxes -->
+    <label>
+      <input v-model="newItemHighPriority" type="checkbox"> Alta Prioridad
+    </label>
+    {{ newItemHighPriority ? "👍" : "👈" }}
+    <!-- boton en la UI -->
+    <button class="btn btn-primary">Salvar articulo</button>
+  </form>
+  <ul>
+    <li v-for="{ id, label } in items" v-bind:key="id">
+      🔹 {{ label }}
+    </li>
+  </ul>
+  <p v-if ="items.length === 0">🥀 Lista de Compras Vacia 🥀</p>
+  <p v-else>😎 Ingrese más Items</p>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<style>
+.shopping-cart-icon {
+  font-size: 2rem;
 }
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.addint {
+  outline: none;
+  border-color: #26376b;
+  color: #3C486B;
+  height: 20px;
+  background-color: rgb(231, 231, 231);
+  border-radius: 8px;
+  border: 1px solid #3C486B;
 }
 </style>
